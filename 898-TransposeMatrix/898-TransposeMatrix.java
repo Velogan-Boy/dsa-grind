eb313263981a611d16bcf49bb70cc81a7ae05d0e
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-27 17:23:09 +0530

    Create 898-TransposeMatrix

---
+++ 898-TransposeMatrix/898-TransposeMatrix.java
@@ -0,0 +1,15 @@
+// Last updated: 2/27/2026, 5:23:09 PM
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int[][] ans = new int[matrix[0].length][matrix.length];
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        
+        return ans;
+    }
+}
