afa3d875a590c2ae30902ec12b7d8a233d1511af
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-27 17:19:20 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
@@ -0,0 +1,35 @@
+// Last updated: 2/27/2026, 5:19:20 PM
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+
+        StringBuilder str = new StringBuilder();
+        str.append(s);
+
+        int[][] dp = new int[s.length()][s.length()];
+
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
+
+        return func(s,str.reverse().toString(),s.length() - 1, s.length() - 1,dp);
+    }
+
+
+    public int func(String text1, String text2, int index1, int index2,int[][]dp){
+
+        if(index1 < 0 || index2 < 0) return 0;
+
+        if(dp[index1][index2] != -1) return dp[index1][index2];
+
+        if(text1.charAt(index1) == text2.charAt(index2))
+            dp[index1][index2] = 1 + func(text1,text2,index1-1,index2-1,dp);
+
+        else
+         dp[index1][index2] =  0 + Math.max(func(text1,text2,index1,index2-1,dp),func(text1,text2,index1-1,index2,dp));
+
+         return dp[index1][index2];
+
+
+
+    }
+}
