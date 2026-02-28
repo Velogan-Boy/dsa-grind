f3a820b1115972fc577c03800f7c79240de88d00
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:26:04 +0530

    Create 1497-DesignAStackWithIncrementOperation

---
+++ 1497-DesignAStackWithIncrementOperation/1497-DesignAStackWithIncrementOperation.py
@@ -0,0 +1,27 @@
+# Last updated: 2/28/2026, 4:26:04 PM
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize
+        
+    def push(self, x: int) -> None:
+        if len(self.stack) != self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack) == 0: return -1
+        return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(len(self.stack)):
+            if i+1 <= k:
+                self.stack[i] += val
+        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
