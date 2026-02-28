0ef150ced9110d965d59d10ebc3c0e984c5d1b42
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:26:30 +0530

    Create 908-MiddleOfTheLinkedList

---
+++ 908-MiddleOfTheLinkedList/908-MiddleOfTheLinkedList.java
@@ -0,0 +1,21 @@
+// Last updated: 2/28/2026, 4:26:30 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+        public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
