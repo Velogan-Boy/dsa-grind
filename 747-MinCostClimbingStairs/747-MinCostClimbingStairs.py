0a517883d59500695d56a9af483a7b32fa0ab962
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:26:37 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.py
@@ -0,0 +1,12 @@
+# Last updated: 2/28/2026, 4:26:36 PM
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+
+        return min(dp[n-1], dp[n-2])
