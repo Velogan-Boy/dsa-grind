02ce71950b751f4c468c34fc730b7120a9525c1f
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 22:14:36 +0530

    Create 2015-DetermineWhetherMatrixCanBeObtainedByRotation

---
+++ 2015-DetermineWhetherMatrixCanBeObtainedByRotation/2015-DetermineWhetherMatrixCanBeObtainedByRotation.py
@@ -0,0 +1,18 @@
+# Last updated: 2/28/2026, 10:14:35 PM
+class Solution(object):
+    def findRotation(self, mat, target):
+        m = len(mat)
+        n = len(mat[0])
+
+        for _ in range(4):
+            for i in range(m):
+                for j in range(i, n):
+                    mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+
+            for row in mat:
+                row.reverse()
+
+            if mat == target:
+                return True
+
+        return False
