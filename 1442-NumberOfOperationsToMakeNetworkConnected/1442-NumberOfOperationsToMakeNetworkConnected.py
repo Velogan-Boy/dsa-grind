257174abc96ffeb16a5bba5f9cf0fc37336bb791
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:26:07 +0530

    Create 1442-NumberOfOperationsToMakeNetworkConnected

---
+++ 1442-NumberOfOperationsToMakeNetworkConnected/1442-NumberOfOperationsToMakeNetworkConnected.py
@@ -0,0 +1,20 @@
+# Last updated: 2/28/2026, 4:26:07 PM
+class Solution(object):
+    def makeConnected(self, n, connections):
+        if len(connections) < n - 1: return -1
+
+        G = [set() for i in range(n)]
+
+        for i, j in connections:
+            G[i].add(j)
+            G[j].add(i)
+            
+        seen = [0] * n
+
+        def dfs(i):
+            if seen[i]: return 0
+            seen[i] = 1
+            for j in G[i]: dfs(j)
+            return 1
+
+        return sum(dfs(i) for i in range(n)) - 1
