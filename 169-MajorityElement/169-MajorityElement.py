ff06cc10186c57cd41086955b0fba5099ab621ad
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:28:13 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.py
@@ -0,0 +1,19 @@
+# Last updated: 2/28/2026, 4:28:13 PM
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        ans = -1
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                ans = num
+            
+            if ans == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return ans
+
+        
