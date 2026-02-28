6c247ace726c9a98d7beb712504d8317446add7d
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-02-28 16:27:22 +0530

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.java
@@ -0,0 +1,13 @@
+// Last updated: 2/28/2026, 4:27:22 PM
+public class Solution {
+    int ans=0, h=0;
+    public int findBottomLeftValue(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return ans;
+    }
+    public void findBottomLeftValue(TreeNode root, int depth) {
+        if (h<depth) {ans=root.val;h=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1);
+    }
+}
