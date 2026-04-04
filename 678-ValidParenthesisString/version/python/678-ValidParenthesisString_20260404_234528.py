2ebc206fc838f362225576a7359389aa3f98815a
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-04-04 23:45:29 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/version/python/678-ValidParenthesisString_20260404_234528.py
@@ -0,0 +1,18 @@
+# Last updated: 4/4/2026, 11:45:28 PM
+1class Solution:
+2    def checkValidString(self, s: str) -> bool:
+3        leftMin, leftMax = 0, 0
+4
+5        for c in s:
+6            if c == "(":
+7                leftMin, leftMax = leftMin + 1, leftMax + 1
+8            elif c == ")":
+9                leftMin, leftMax = leftMin - 1, leftMax - 1
+10            else:
+11                leftMin, leftMax = leftMin - 1, leftMax + 1
+12            if leftMax < 0:
+13                return False
+14            if leftMin < 0:  # required because -> s = ( * ) (
+15                leftMin = 0
+16                
+17        return leftMin == 0
