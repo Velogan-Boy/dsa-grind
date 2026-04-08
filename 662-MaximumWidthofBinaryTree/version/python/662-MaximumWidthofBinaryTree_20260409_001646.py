30dcd1b35c3fa4c0de017c324aa83f0f74e86658
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-04-09 00:16:47 +0530

    Create 662-MaximumWidthofBinaryTree

---
+++ 662-MaximumWidthofBinaryTree/version/python/662-MaximumWidthofBinaryTree_20260409_001646.py
@@ -0,0 +1,25 @@
+# Last updated: 4/9/2026, 12:16:46 AM
+1from collections import deque
+2from math import inf
+3
+4class Solution:
+5    def widthOfBinaryTree(self, root):
+6        q = deque([(root, 0)]) 
+7        ans = 0
+8
+9        while q:
+10            size = len(q)
+11            _, first = q[0]
+12            _, last = q[-1]
+13
+14            ans = max(ans, last - first + 1)
+15
+16            for _ in range(size):
+17                node, idx = q.popleft()
+18
+19                if node.left:
+20                    q.append((node.left, 2 * idx))
+21                if node.right:
+22                    q.append((node.right, 2 * idx + 1))
+23
+24        return ans
