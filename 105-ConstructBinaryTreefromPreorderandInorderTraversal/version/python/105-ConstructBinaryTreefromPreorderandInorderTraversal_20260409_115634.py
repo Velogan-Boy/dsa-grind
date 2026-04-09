ea84440d6d228d07a6136cc59677c00fa2ab2cd9
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-04-09 11:56:35 +0530

    Create 105-ConstructBinaryTreefromPreorderandInorderTraversal

---
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/version/python/105-ConstructBinaryTreefromPreorderandInorderTraversal_20260409_115634.py
@@ -0,0 +1,15 @@
+# Last updated: 4/9/2026, 11:56:34 AM
+1class Solution:
+2    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+3        if not preorder or not inorder:
+4            return None
+5
+6        root = TreeNode(preorder[0])
+7        mid = inorder.index(preorder[0])
+8
+9        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+10        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+11
+12        return root
+13        
+14        
