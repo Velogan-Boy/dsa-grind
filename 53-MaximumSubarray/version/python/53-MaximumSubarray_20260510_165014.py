7f64410dcf8b8f5c745f5aad13c1697dccccd317
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-05-10 16:50:14 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/version/python/53-MaximumSubarray_20260510_165014.py
@@ -0,0 +1,15 @@
+# Last updated: 5/10/2026, 4:50:14 PM
+1class Solution:
+2    def maxSubArray(self, nums: List[int]) -> int:
+3        n = len(nums)
+4
+5        currSum = 0
+6        maxSum = -inf
+7        for num in nums:
+8            currSum += num
+9            maxSum = max(maxSum, currSum)
+10            if currSum < 0: currSum = 0
+11        
+12        return maxSum
+13
+14        
