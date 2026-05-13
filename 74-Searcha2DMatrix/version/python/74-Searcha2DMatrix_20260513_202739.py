f35dec381b44ef0b2443a27c6b21bedab140e2a3
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-05-13 20:27:40 +0530

    Create 74-Searcha2DMatrix

---
+++ 74-Searcha2DMatrix/version/python/74-Searcha2DMatrix_20260513_202739.py
@@ -0,0 +1,22 @@
+# Last updated: 5/13/2026, 8:27:39 PM
+1class Solution:
+2    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+3
+4        m,n = len(matrix), len(matrix[0])
+5        s, e = 0, m * n - 1
+6
+7        while s <= e:
+8            mid = (s + e) // 2
+9            row = mid // n
+10            col = mid % n
+11
+12            if matrix[row][col] == target: return True
+13            elif matrix[row][col] < target:
+14                s = mid + 1
+15            else:
+16                e = mid - 1
+17
+18        return False
+19
+20
+21        
