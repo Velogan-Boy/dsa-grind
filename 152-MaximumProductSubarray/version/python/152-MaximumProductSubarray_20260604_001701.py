01654475b4459f2c2f24acc38c489efe583d8cee
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-06-04 00:17:01 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/version/python/152-MaximumProductSubarray_20260604_001701.py
@@ -0,0 +1,24 @@
+# Last updated: 6/4/2026, 12:17:01 AM
+1class Solution:
+2    def maxProduct(self, arr):
+3        n = len(arr)
+4
+5        pre, suff = 1, 1
+6
+7        ans = float('-inf')
+8
+9        for i in range(n):
+10            if pre == 0:
+11                pre = 1
+12
+13            if suff == 0:
+14                suff = 1
+15
+16            pre *= arr[i]
+17
+18            suff *= arr[n - i - 1]
+19
+20            ans = max(ans, pre, suff)
+21
+22        return ans
+23
