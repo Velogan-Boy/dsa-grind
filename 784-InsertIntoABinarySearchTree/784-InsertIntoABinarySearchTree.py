5c6dbc8024cf98857a674b4fd9020fab18b7dd0d
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:18:20 +0530

    Create 784-InsertIntoABinarySearchTree

---
+++ 784-InsertIntoABinarySearchTree/784-InsertIntoABinarySearchTree.py
@@ -0,0 +1,32 @@
+# Last updated: 7/12/2026, 6:18:19 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        if not root: return TreeNode(val)
+
+        curr = root
+
+        while curr:
+            if curr.val < val:
+                if not curr.right:
+                    curr.right = TreeNode(val)
+                    break
+
+                curr = curr.right
+            else:
+                if not curr.left:
+                    curr.left = TreeNode(val)
+                    break
+
+                curr = curr.left
+            
+        return root
+
+
+        
