64a1327c4b82e95d37f3f2aaf5b7fad48d7806ae
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:19:19 +0530

    Create 539-MinimumTimeDifference

---
+++ 539-MinimumTimeDifference/539-MinimumTimeDifference.py
@@ -0,0 +1,20 @@
+# Last updated: 7/12/2026, 6:19:19 PM
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        def timeToMinutes(time: str) -> int:
+            hours, minutes = map(int, time.split(':'))
+            return hours * 60 + minutes
+        
+        minutes = [timeToMinutes(tp) for tp in timePoints]
+        
+        minutes.sort()
+        
+        min_diff = float('inf')  # Initialize to a large number
+        
+        for i in range(1, len(minutes)):
+            min_diff = min(min_diff, minutes[i] - minutes[i - 1])
+        
+        circular_diff = 1440 - minutes[-1] + minutes[0]
+        min_diff = min(min_diff, circular_diff)
+        
+        return min_diff
