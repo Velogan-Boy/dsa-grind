a8f7213b1ce98b09fbd3baa4a60908c142b36ba4
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:19:04 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.py
@@ -0,0 +1,18 @@
+# Last updated: 7/12/2026, 6:19:04 PM
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        empty = 0
+        f = [0] + flowerbed + [0]
+
+        for i in range(1, len(f) - 1):
+            if f[i] == 1: continue
+
+            if f[i - 1] == 0 and f[i + 1] == 0:
+                empty+=1
+                f[i] = 1
+
+        return empty >= n
+        
+
+
+        
