c25ab25c9bc29becbabfdf0b9aed9940c12375b2
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:21:33 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2026, 6:21:33 PM
+class Solution:
+    def reverseBits(self, n: int) -> int:
+
+        res = 0
+
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
+
+        return res
+
+       
+        
