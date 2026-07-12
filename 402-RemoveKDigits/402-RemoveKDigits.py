63a32f030ce9bff420cadfb20dc563d7bec4567c
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:19:53 +0530

    Create 402-RemoveKDigits

---
+++ 402-RemoveKDigits/402-RemoveKDigits.py
@@ -0,0 +1,17 @@
+# Last updated: 7/12/2026, 6:19:53 PM
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for i in num:
+            while stack and stack[-1] > i and k > 0:
+                k -= 1
+                stack.pop()
+
+            if stack or i is not "0":
+                stack.append(i)
+
+        if k:
+            stack = stack[:-k]
+            
+        return ''.join(stack) or '0'
+        
