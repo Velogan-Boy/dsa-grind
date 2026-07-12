1c293d22c98e12a7d7c3183b14b181a736f2f374
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:22:22 +0530

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/138-CopyListWithRandomPointer.py
@@ -0,0 +1,51 @@
+# Last updated: 7/12/2026, 6:22:22 PM
+# class ListNode:
+#     def __init__(self, val=0, next=None, random=None):
+#         self.val = val
+#         self.next = next
+#         self.random = random
+
+class Solution:
+    def insertCopyInBetween(self, head):
+        temp = head
+        while temp:
+            nextElement = temp.next
+            copy = ListNode(temp.val)
+            
+            copy.next = nextElement
+            temp.next = copy
+            temp = nextElement
+
+    def connectRandomPointers(self, head):
+        temp = head
+        while temp:
+            copyNode = temp.next
+            
+            if temp.random:
+                copyNode.random = temp.random.next
+            else:
+                copyNode.random = None
+            
+            temp = temp.next.next
+
+    def getDeepCopyList(self, head):
+        temp = head
+        dummyNode = ListNode(-1)
+        res = dummyNode
+
+        while temp:
+            res.next = temp.next
+            res = res.next
+
+            temp.next = temp.next.next
+            temp = temp.next
+        
+        return dummyNode.next
+
+    def copyRandomList(self, head):
+        if not head: return None
+
+        self.insertCopyInBetween(head)
+        self.connectRandomPointers(head)
+        return self.getDeepCopyList(head)
+
