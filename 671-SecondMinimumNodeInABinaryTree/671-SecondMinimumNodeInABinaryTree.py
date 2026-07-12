84506bbfb50c5affd807d00e86174a8cf9f17a31
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:18:46 +0530

    Create 671-SecondMinimumNodeInABinaryTree

---
+++ 671-SecondMinimumNodeInABinaryTree/671-SecondMinimumNodeInABinaryTree.py
@@ -0,0 +1,23 @@
+# Last updated: 7/12/2026, 6:18:46 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        self.ans = float('inf')
+        min1 = root.val
+
+        def dfs(node):
+            if node:
+                if min1 < node.val < self.ans:
+                    self.ans = node.val
+                elif node.val == min1:
+                    dfs(node.left)
+                    dfs(node.right)
+
+        dfs(root)
+        return self.ans if self.ans < float('inf') else -1
+        
