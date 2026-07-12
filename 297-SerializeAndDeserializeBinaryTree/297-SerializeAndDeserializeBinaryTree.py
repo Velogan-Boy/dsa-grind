6a8420684e4d1940e32cdf05ad8872d881a89f01
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:20:21 +0530

    Create 297-SerializeAndDeserializeBinaryTree

---
+++ 297-SerializeAndDeserializeBinaryTree/297-SerializeAndDeserializeBinaryTree.py
@@ -0,0 +1,40 @@
+# Last updated: 7/12/2026, 6:20:21 PM
+class Codec:
+    def serialize(self, root):
+        if not root: return "null"
+
+        res, q = [], deque([root])
+
+        while q:
+            node = q.popleft()
+            if node:
+                res.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                res.append("null")
+
+        return ','.join(res)
+
+    def deserialize(self, data):
+        if data == "null": return None
+
+        nodes = data.split(',')
+        root = TreeNode(int(nodes[0]))
+        q = deque([root])
+
+        i = 1
+        while q:
+            curr = q.popleft()
+
+            if nodes[i] != "null":
+                curr.left = TreeNode(int(nodes[i]))
+                q.append(curr.left)
+            i += 1
+
+            if i < len(nodes) and nodes[i] != "null":
+                curr.right = TreeNode(int(nodes[i]))
+                q.append(curr.right)
+            i += 1
+            
+        return root
