9365be819852a34d90e2463b4903b66bc4f303eb
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:21:08 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.py
@@ -0,0 +1,11 @@
+# Last updated: 7/12/2026, 6:21:08 PM
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        last_seen = {}
+
+        for i, num in enumerate(nums):
+            if num in last_seen and i - last_seen[num] <= k:
+                return True
+            last_seen[num] = i
+
+        return False
