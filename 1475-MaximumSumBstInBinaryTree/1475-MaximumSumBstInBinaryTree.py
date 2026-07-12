04409e1270604641548bf66903130373fb520dca
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-12 18:17:02 +0530

    Create 1475-MaximumSumBstInBinaryTree

---
+++ 1475-MaximumSumBstInBinaryTree/1475-MaximumSumBstInBinaryTree.py
@@ -0,0 +1,28 @@
+# Last updated: 7/12/2026, 6:17:02 PM
+class Solution:
+    def maxSumBST(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+
+        def dfs(node):
+            nonlocal ans
+            if not node:
+                return inf, -inf, 0
+            
+            lmin,lmax,lsum = dfs(node.left)
+            rmin,rmax,rsum = dfs(node.right)
+
+            if lmax < node.val < rmin:
+                currSum = lsum + rsum + node.val
+                ans = max(ans, currSum)
+
+                return min(node.val, lmin), max(node.val, rmax), currSum
+
+            else:
+                return -inf, inf, 0
+        
+        dfs(root)
+
+        return ans
+
+            
+        
