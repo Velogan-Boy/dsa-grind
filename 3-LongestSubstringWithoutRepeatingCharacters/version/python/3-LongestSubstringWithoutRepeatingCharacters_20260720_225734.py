055a75ce2f28a17251e863df13ad7aad6a95d9d1
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-07-20 22:57:34 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/python/3-LongestSubstringWithoutRepeatingCharacters_20260720_225734.py
@@ -0,0 +1,15 @@
+# Last updated: 7/20/2026, 10:57:34 PM
+1class Solution:
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        hm = {}
+4        i = 0
+5        ans = 0
+6
+7        for j in range(len(s)):
+8            if s[j] in hm:
+9                i = max(i, hm[s[j]] + 1)
+10
+11            hm[s[j]] = j
+12            ans = max(ans, j - i + 1)
+13
+14        return ans
