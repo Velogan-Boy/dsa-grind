b3c8bd0073fa79bed57ae80b2c2262b609de160c
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-08-02 23:49:40 +0530

    Create 3014-MinimumNumberofPushestoTypeWordI

---
+++ 3014-MinimumNumberofPushestoTypeWordI/version/python/3014-MinimumNumberofPushestoTypeWordI_20260802_234940.py
@@ -0,0 +1,17 @@
+# Last updated: 8/2/2026, 11:49:40 PM
+1from collections import defaultdict
+2
+3class Solution:
+4    def minimumPushes(self, word: str) -> int:
+5        freq = defaultdict(int)
+6
+7        for ch in word:
+8            freq[ch] += 1
+9
+10        frequencies = sorted(freq.values(), reverse=True)
+11
+12        ans = 0
+13        for i, f in enumerate(frequencies):
+14            ans += f * (i // 8 + 1)
+15
+16        return ans
