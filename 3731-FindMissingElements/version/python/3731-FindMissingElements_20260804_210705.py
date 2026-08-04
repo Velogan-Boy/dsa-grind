ea28cf810fc2acab396fe200226d2d0f8b7bc79b
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2026-08-04 21:07:06 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/version/python/3731-FindMissingElements_20260804_210705.py
@@ -0,0 +1,19 @@
+# Last updated: 8/4/2026, 9:07:05 PM
+1class Solution:
+2    def findMissingElements(self, nums: List[int]) -> List[int]:
+3
+4        mini = min(nums)
+5        maxi = max(nums)
+6
+7        hashSet = set(nums)
+8
+9        ans = []
+10        for num in range(mini, maxi + 1):
+11            if num not in hashSet:
+12                ans.append(num)
+13
+14        return ans
+15
+16
+17
+18        
